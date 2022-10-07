7b4f40d9e3bfddfd4fd68e72d8e4f083167418ed
Gabriel Ferreira Sasdelli <sasdelle69@gmail.com>
2022-10-07 16:09:21 -0300

    Teste de validação de email (retrono de valido e invalido)

---
+++ tests/Sample.Test/Extensions/StringExtensionsTest.cs
@@ -0,0 +1,38 @@
+﻿using Sample.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Sample.Test.Extensions
+{
+    public class StringExtensionsTest
+    {
+        public StringExtensionsTest() { }
+
+        [Theory]
+        [InlineData("sasdellegabs")]
+        [InlineData("sasdellegabs@gmail")]
+        public void IsValidMail_ReturnFalse(string expectedEmail)
+        {
+            var email = expectedEmail;
+
+            var emailValid = email.IsValidMail();
+            
+            Assert.False(emailValid);
+        }
+
+        [Theory]
+        [InlineData("sasdellegabs@gmail.com")]
+        [InlineData("sasdellegabs@yahoo.com")]
+        public void IsValidMail_ReturnTrue(string expectedEmail)
+        {
+            var email = expectedEmail;
+
+            var emailValid = email.IsValidMail();
+
+            Assert.True(emailValid);
+        }
+
+    }
+}
