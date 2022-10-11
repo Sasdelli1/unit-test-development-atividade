0e55da81e00fa9f2264686868810f7d06121c6ab
Gabriel Ferreira Sasdelli <sasdelle69@gmail.com>
2022-10-10 22:54:51 -0300

    ToStringMoney faz teste com sucesso

--- tests/Sample.Test/Extensions/DoubleExtensionsTest.cs
+++ tests/Sample.Test/Extensions/DoubleExtensionsTest.cs
@@ -1,4 +1,5 @@
-﻿using Sample.Extensions;
+﻿using Newtonsoft.Json.Linq;
+using Sample.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,17 +13,20 @@ namespace Sample.Test.Extensions
         { 
         }
 
-        //[Theory]
-        //[InlineData(10.20)]
-        //[InlineData(40.60)]
-        //public void ToStringMoneyPtBR(double value)
-        //{
-        //    //Arrange
-        //    string actualResult = "";
-        //    //Act
-        //    double doubleAmt = double.Parse(value.ToStringMoneyPtBR());
-        //    //Assert
-        //    Assert.True( actualResult);
-        //}
+        [Theory]
+        [InlineData(5.99)]
+        [InlineData(2.99)]
+        [InlineData(5000.00)]
+        [InlineData(45.80)]
+        public void ToStringMoneyPtBR(double value)
+        {
+
+            string actualResult;
+
+            //double doubleAmt = double.Parse(value.ToStringMoneyPtBR());
+            actualResult = value.ToStringMoneyPtBR();
+
+            Assert.Contains("R$", actualResult);
+        }
     }
 }
